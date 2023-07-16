fd84b50e0c891290c4816c8f448d29431105e28e
mikechege01 <mikechege93@gmail.com>
2023-07-16 12:59:36 -0700

    Class Place inherits from BaseClass

---
+++ models/place.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Class Place inherits from BaseModel
+"""
+
+
+import uuid
+from models.base_model import BaseModel
+import datetime
+
+
+class Place(BaseModel):
+    """
+    Class Place definition
+    """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """
+        Instantiation method for class Place
+        """
+        super().__init__(*args, **kwargs)
